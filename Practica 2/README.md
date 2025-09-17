c67910e9e63492065f8ea01a785a482e058b62b2
vectorwar <al23760817@ite.edu.mx>
2025-09-16 20:48:10 -0700

    Creacion de la practica 2

---
+++ Practica 2/README.md
@@ -0,0 +1,7 @@
+# Practica 2 movimiento 
+
+En esta practica, seguiremos utilizando el modulo de la tortuga, en este caso, para generar una figura (cuadrado), y darle movimiento con ciertos metodos, yo utilice setheading que se basa en los grados de orientacion que le de para moverse segun las teclas, de igual manera se puede utilizar una orientacion x,y y darle una distancia, pero a mi me gusto mas el metodo que utilice. Lo encontre en paginas de documentacion como la oficial de python-tirtle, DevDocs, y videos de youtube. Para conseguir que la figura se mueva sin arrastrar el lapiz, utilice clear en cada funcion de una de las teclas, para que cada que se arrastre vaya limpiando, de igual manera el metodo onkey que recibe una funcion y letra como un string para poder definirlo como la flecha de orientacion . Implemente buenas practicas de docstring que encontre tambien en la documentacion de python. Esta practica me ayudo a comprender mejor como se manejan los metodos e imagenes con este modulo de python.
+
+Esta practica se realizo con la version 3.10.11 de python.
+
+NOTA: Me podria dar una retroalimentacion de cada practica para mejorar y evitar futuros errores, para tener mejores practicas de programacion.
---
+++ Practica 2/practica2_movimiento6.py
@@ -0,0 +1,76 @@
+"""Este codigo mueve un objeto en la pantalla usando las teclas de flecha."""
+"""Autor: Jose Juan Padilla B"""
+import turtle as t
+
+t.title("Mover con las flechas el cuadrado")
+
+"""Configuracion de la pantalla"""
+PANTALLA = t.Screen()
+PANTALLA.setup(600, 600)
+PANTALLA.bgcolor("lightblue")
+lado = 90 #Parametro global para el lado del cuadrado
+
+def teleport (x:float, y:float): #Segui usando el teleport para dibujar mi figura en cierto punto
+    """Aqui movemos el cuadrado a la posicion (x, y)"""
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+
+def cuadrado (lado):
+    """Aqui dibujamos un cuadrado tomando el lado especificado."""
+    t.color("black", "yellow")
+    t.fillcolor("yellow")
+    t.begin_fill()
+    for _ in range(4):
+        t.forward(lado)
+        t.right(90)
+    t.end_fill()
+def w():
+    """Mover hacia arriba"""
+    t.penup()
+    t.setheading(90) #Este metodo de la torguga gira la tortuga a un angulo especifico segun la direccion de las agujas del reloj.
+    t.fd(50)
+    t.clear() # El metodo clear limpia los dibujos hechos por la tortuga.
+    cuadrado(lado)
+
+def s():
+    """Mover hacia abajo"""
+    t.penup()
+    t.setheading(270)
+    t.fd(50)
+    t.clear()
+    cuadrado(lado)
+def a():
+    """Mover hacia la izquierda"""
+    t.penup()
+    t.setheading(180)
+    t.fd(50)
+    t.clear()
+    cuadrado(lado)
+def d():
+    """Mover hacia la derecha"""
+    t.penup()
+    t.setheading(0)
+    t.fd(50)
+    t.clear()
+    cuadrado(90)
+
+def controles_teclas():
+    """Aqui definimos los controles del teclado."""
+    PANTALLA.listen()
+    PANTALLA.onkey(w, "Up") #El metodo onkey recibe una funcion y una tecla en string para definirla como un control, en este caso las flechas.
+    PANTALLA.onkey(s, "Down")
+    PANTALLA.onkey(a, "Left")
+    PANTALLA.onkey(d, "Right")
+    
+def main():
+    """Funcion principal"""
+    teleport(-50, 100)
+    cuadrado(lado)
+    t.speed(0)
+    controles_teclas()
+    PANTALLA.mainloop()
+    
+
+if __name__ == "__main__":
+     main()
